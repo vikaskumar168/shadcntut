08969b1205821b0c660997c39dea94a4cedd822f
vikas <vikaskmr2411@gmail.com>
2024-12-23 02:01:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+import { Carousel } from "./components/ui/carousel";
+import { Card } from "./components/ui/Card";
+
+const App = () => {
+  return (
+    <>
+      <Button>Hello shadcn</Button>
+      <Carousel>Hello shadcn</Carousel>
+      <Card>Hello shadcn</Card>
+    </>
+  );
+};
+
+export default App;
